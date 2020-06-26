7f1d018beee4712c42937b0850d44605624e1eba
Firoms <edisonmessi@naver.com>
2020-06-26 21:44:22 +0900

    API

--- Get_Api.py
+++ Get_Api.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     # r = requests.get(
     #     'https://data.football-api.com/v3/competitions?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
     # data = r.json()
+
     db = sqlite3.connect("DB/Football_API.db")
     cursor = db.cursor()
+
     # for i in range(len(data)):
     #     leauge_data = data[i]
     #     leauge_id = leauge_data['id']
