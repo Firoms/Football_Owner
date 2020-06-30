94ab5e9d9072efdd894d6a053e6e35e69110e603
Firoms <edisonmessi@naver.com>
2020-06-30 18:37:52 +0900

    Working on Savefiles

--- Get_Savefiles.py
+++ Get_Savefiles.py
@@ -1,21 +1,37 @@
 import sqlite3
+from tkinter import *
+import tkinter.messagebox
 from tkinter.simpledialog import askstring
 from tkinter.messagebox import showinfo
 import time
 
 
-def Get_Savefiles():
-    db = sqlite3.connect("DB/Football_API_Test.db")
+def Check_Savefiles(savefile):
+    db = sqlite3.connect(f"DB/FO_savefile{savefile}.db")
     cursor = db.cursor()
     cursor.execute(
         "SELECT * FROM Gamer ORDER BY Date DESC")
-    Save_list1 = cursor.fetchone()
-    Save_list2 = cursor.fetchone()
-    Save_list3 = cursor.fetchone()
-    return Save_list1, Save_list2, Save_list3
+    Save_list = cursor.fetchone()
+    return Save_list
 
 
-def input_Names():
+def input_Names1():
+    name = askstring('구단주 생성', '구단주 이름을 입력하세요.')
+    now = time.localtime()
+    Date = ("%04d.%02d.%02d.%02d.%02d.%02d" % (now.tm_year, now.tm_mon,
+                                               now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
+    if name == None:
+        return 'No'
+    else:
+        db = sqlite3.connect("DB/FO_savefile1.db")
+        cursor = db.cursor()
+        insert_query = \
+            f"INSERT INTO Gamer VALUES('{name}',  '무직', '{Date}')"
+        cursor.execute(insert_query)
+        db.commit()
+
+
+def input_Names2():
     name = askstring('구단주 생성', '구단주 이름을 입력하세요.')
     now = time.localtime()
     Date = ("%04d.%02d.%02d.%02d.%02d.%02d" % (now.tm_year, now.tm_mon,
@@ -23,9 +39,23 @@ def input_Names():
     if name == None:
         pass
     else:
-        db = sqlite3.connect("DB/Football_API_Test.db")
+        db = sqlite3.connect("DB/FO_savefile2.db")
         cursor = db.cursor()
         insert_query = \
-            f"INSERT INTO Gamer VALUES('{name}',  'not employed', '{Date}')"
+            f"INSERT INTO Gamer VALUES('{name}',  '무직', '{Date}')"
         cursor.execute(insert_query)
         db.commit()
+
+
+def reset_datas():
+    Warning_message = tkinter.messagebox.askokcancel(
+        "경고", "정말 모든 데이터 파일을 삭제하시겠습니까?")
+    if Warning_message == True:
+        for i in range(3):
+            db = sqlite3.connect(f"DB/FO_savefile{i+1}.db")
+            cursor = db.cursor()
+            cursor.execute("DELETE FROM Gamer")
+            cursor.execute("DELETE FROM Players")
+            cursor.execute("DELETE FROM Statistics")
+            cursor.execute("DELETE FROM Team_Datas")
+            db.commit()
--- Screen.py
+++ Screen.py
@@ -1,6 +1,7 @@
 from Get_Image import Get_image
 from Get_Savefiles import *
 from tkinter import *
+import time
 
 
 class Screen:
@@ -12,14 +13,14 @@ class Screen:
         Menu_Screen_background = Get_image.image_label(
             self.Gui, "Main_Screen_bg.png", 0, 0)
         Game_Start_button = Get_image.image_button(
-            self.Gui, "Game_Start_btn.png", 20, 350, self.Savefiles_Screen)
+            self.Gui, "Game_Start_btn.png", 20, 350, self.First_Savefiles_Screen)
         HowToPlay_button = Get_image.image_button(
             self.Gui, "HowToPlay_btn.png", 20, 450, self.Incomplete_Screen)
         Settings_button = Get_image.image_button(
             self.Gui, "Settings_btn.png", 20, 550, self.Incomplete_Screen)
         Exit_button = Get_image.image_button(
             self.Gui, "Exit_btn.png", 20, 650, self._quit)
-        Version_label = Label(self.Gui.Gui, text="Version 0.04",
+        Version_label = Label(self.Gui.Gui, text="Version 0.05",
                               fg="green", font=("맑은 고딕", 12), height=1)
         Version_label.place(x=1100, y=5)
 
@@ -28,16 +29,21 @@ class Screen:
             self.Gui, "Incomplete_bg.png", 0, 0)
         Return_button = Get_image.image_button(
             self.Gui, "Return_btn.png", 920, 10, self.Menu_Screen)
+        reset_datas_btn = Button(self.Gui.Gui, text="reset datas",
+                                 bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21, command=reset_datas)
+        reset_datas_btn.place(x=75, y=198)
 
     def Savefiles_Screen(self):
-        Incomplete_Screen_background = Get_image.image_label(
+        Savefiles_Screen_background = Get_image.image_label(
             self.Gui, "Savefiles_bg.png", 0, 0)
         Return_button = Get_image.image_button(
             self.Gui, "Return_btn.png", 885, 30, self.Menu_Screen)
-        Save_check1, Save_check2, Save_check3 = Get_Savefiles()
+        Save_check1 = Check_Savefiles(1)
+        Save_check2 = Check_Savefiles(2)
+        Save_check3 = Check_Savefiles(3)
         if Save_check1 == None:
             first_save_btn = Button(self.Gui.Gui, text="+",
-                                    bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21, command=input_Names)
+                                    bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21)
             first_save_btn.place(x=75, y=198)
         else:
             first_save_btn = Button(self.Gui.Gui, text=Save_check1,
@@ -45,20 +51,44 @@ class Screen:
             first_save_btn.place(x=75, y=198)
         if Save_check2 == None:
             second_save_btn = Button(self.Gui.Gui, text="+",
-                                     bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21, command=input_Names)
+                                     bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21)
             second_save_btn.place(x=75, y=402)
         else:
             second_save_btn = Button(self.Gui.Gui, text=Save_check2,
                                      bg="yellowgreen", fg="blue", font=("맑은 고딕", 20), width=48, height=3)
             second_save_btn.place(x=75, y=402)
         if Save_check3 == None:
-            third_save_btn = Button(self.Gui.Gui, text="+",
-                                    bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21, command=input_Names)
+            third_save_btn = Button(self.Gui.Gui, text="자동 저장 된 정보가 없습니다.",
+                                    bg="yellowgreen", fg="blue", font=("맑은 고딕", 20), width=48, height=3)
             third_save_btn.place(x=75, y=606)
         else:
             third_save_btn = Button(self.Gui.Gui, text=Save_check3,
                                     bg="yellowgreen", fg="blue", font=("맑은 고딕", 20), width=48, height=3)
             third_save_btn.place(x=75, y=606)
+
+    def First_Savefiles_Screen(self):
+        Save_check1 = Check_Savefiles(1)
+        if Save_check1 == None:
+            Savefiles_Screen_background = Get_image.image_label(
+                self.Gui, "Savefiles_bg.png", 0, 0)
+            Return_button = Get_image.image_button(
+                self.Gui, "Return_btn.png", 885, 30, self.Menu_Screen)
+
+            first_save_btn = Button(self.Gui.Gui, text="+",
+                                    bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21)
+            first_save_btn.place(x=75, y=198)
+            second_save_btn = Button(self.Gui.Gui, text="+",
+                                     bg="yellowgreen", fg="blue", font=("맑은 고딕", 45), width=21)
+            second_save_btn.place(x=75, y=402)
+            third_save_btn = Button(self.Gui.Gui, text="자동 저장 된 정보가 없습니다.",
+                                    bg="yellowgreen", fg="blue", font=("맑은 고딕", 20), width=48, height=3)
+            third_save_btn.place(x=75, y=606)
+            First_Save = input_Names1()
+            if First_Save == 'No':
+                time.sleep(0.3)
+                Return_MainScreen = self.Menu_Screen()
+        else:
+            Savefile_Screen = self.Savefiles_Screen()
 
     def _quit(self):
         answer = messagebox.askyesno("확인", "정말 종료하시겠습니까?")
