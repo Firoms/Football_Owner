37623361ca754e0aba0d487a371f7fd956988c93
Firoms <edisonmessi@naver.com>
2020-07-17 13:53:11 +0900

    working

--- Game_System.py
+++ Game_System.py
@@ -1,44 +1,77 @@
 import sqlite3
+import random
 
 
-def change_values():
-    db = sqlite3.connect(f"DB/FO_datafile.db")
+def change_ages():
+    db = sqlite3.connect(f"DB/FO_datafile1.db")
     cursor = db.cursor()
-    cursor.execute(
-        "UPDATE Players SET Age='Apr 22, 1996 (24)'  WHERE Age == '- (-)'")
+    cursor1 = db.cursor()
+    # cursor.execute(
+    #     "SELECT Seq, Age From Players")
+    # for i in range(56297):
+    #     a = cursor.fetchone()
+    #     Seq = a[0]
+    #     Age = a[1][-3:-1]
+    cursor1.execute(
+        f"UPDATE Players SET Age='23'  WHERE Age == '(-'")
     db.commit()
 
 
-def get_max():
-    db = sqlite3.connect(f"DB/FO_datafile.db")
+def change_values():
+    db = sqlite3.connect(f"DB/FO_datafile1.db")
     cursor = db.cursor()
     cursor.execute(
-        "SELECT Age From Players")
-    a = cursor.fetchone()[0][-3:-1]
-    max_age = int(a)
-    min_age = int(a)
-    for i in range(59293):
-        a = int(cursor.fetchone()[0][-3:-1])
-        if max_age < a:
-            max_age = a
-        if min_age > a:
-            min_age = a
-    print(max_age, min_age)
+        "UPDATE Players Set Market_Value ='€0.00m' Where Market_Value like '%Th.' OR Market_Value == '-' OR Market_Value == ''")
+    db.commit()
+
+
+def delete_same():
+    name = []
+    team = []
+    number = []
+    position = []
+    age = []
+    market_value = []
+
+    db = sqlite3.connect(f"DB/FO_datafile1.db")
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM(SELECT Name, Team, Number, Position, Age, Market_Value, Count(*) Cnt	FROM Players GROUP by Name, Team, Age) WHERE Cnt>1")
+    for i in range(2602):
+        a = cursor.fetchone()
+        name.append(a[0])
+        team.append(a[1])
+        number.append(a[2])
+        position.append(a[3])
+        age.append(a[4])
+        market_value.append(a[5])
+    print("delete 시작")
+    for i in range(2602):
+        cursor.execute(
+            f'DELETE FROM Players WHERE Name=="{name[i]}" and Team=="{team[i]}" and Age=="{age[i]}"')
+        db.commit()
+    print("insert 시작")
+    for i in range(2602):
+        insert_query = \
+            f'INSERT INTO Players VALUES("{name[i]}", "{team[i]}", "{number[i]}","{position[i]}","{age[i]}","{market_value[i]}")'
+        cursor.execute(insert_query)
+        db.commit()
 
 
 def make_row_num():
-    db = sqlite3.connect(f"DB/FO_datafile.db")
+    db = sqlite3.connect(f"DB/FO_datafile1.db")
     cursor = db.cursor()
 
-    for i in range(59293):
+    for i in range(56297):
         cursor.execute(
             f"SELECT * FROM Players ORDER By Field9 LIMIT 1 OFFSET {i}")
         a = cursor.fetchone()
         name = a[1]
         team = a[2]
+        age = a[5]
 
         cursor.execute(
-            f'UPDATE Players SET Seq = {i+1} Where Name = "{name}" AND Team = "{team}"')
+            f'UPDATE Players SET Seq = {i+1} Where Name = "{name}" AND Team = "{team}" AND Age = "{age}"')
         db.commit()
 
 
@@ -46,7 +79,7 @@ def make_ablity():
     Ability_data = []
     Value_data = []
 
-    db = sqlite3.connect(f"DB/FO_datafile.db")
+    db = sqlite3.connect(f"DB/FO_datafile1.db")
     cursor = db.cursor()
     # cursor.execute(
     #     f"UPDATE Players SET Ability='0'")
@@ -99,28 +132,65 @@ def make_ablity():
     for i in range(26, 30):
         for r in range(len(Ability_data)):
             cursor.execute(
-                f"UPDATE Players SET Ability='{i-23+Ability_data[r]}'  WHERE Age like '%({i})' AND Market_value == '€{Value_data[r]}0m'")
+                f"UPDATE Players SET Ability='{i-33+Ability_data[r]}'  WHERE Age == '{i}' AND Market_value == '€{Value_data[r]}0m'")
             db.commit()
     for i in range(30, 36):
         for r in range(len(Ability_data)):
             cursor.execute(
-                f"UPDATE Players SET Ability='{i-26+Ability_data[r]}'  WHERE Age like '%({i})' AND Market_value == '€{Value_data[r]}0m'")
+                f"UPDATE Players SET Ability='{i-36+Ability_data[r]}'  WHERE Age == '{i}' AND Market_value == '€{Value_data[r]}0m'")
             db.commit()
     for i in range(36, 55):
         for r in range(len(Ability_data)):
             cursor.execute(
-                f"UPDATE Players SET Ability='{i-12+Ability_data[r]}'  WHERE Age like '%({i})' AND Market_value == '€{Value_data[r]}0m'")
+                f"UPDATE Players SET Ability='{i-22+Ability_data[r]}'  WHERE Age == '{i}' AND Market_value == '€{Value_data[r]}0m'")
             db.commit()
     for i in range(22, 26):
         for r in range(len(Ability_data)):
             cursor.execute(
-                f"UPDATE Players SET Ability='{27-i+Ability_data[r]}'  WHERE Age like '%({i})' AND Market_value == '€{Value_data[r]}0m'")
+                f"UPDATE Players SET Ability='{17-i+Ability_data[r]}'  WHERE Age == '{i}' AND Market_value == '€{Value_data[r]}0m'")
             db.commit()
     for i in range(14, 22):
         for r in range(len(Ability_data)):
             cursor.execute(
-                f"UPDATE Players SET Ability='{25-i+Ability_data[r]}'  WHERE Age like '%({i})' AND Market_value == '€{Value_data[r]}0m'")
+                f"UPDATE Players SET Ability='{13-i+Ability_data[r]}'  WHERE Age == '{i}' AND Market_value == '€{Value_data[r]}0m'")
             db.commit()
 
 
-make_row_num()
+def make_potential():
+    db = sqlite3.connect(f"DB/FO_datafile1.db")
+    cursor = db.cursor()
+    cursor1 = db.cursor()
+    cursor.execute(
+        "SELECT Seq, Age, Ability, Market_Value From Players")
+    for i in range(56297):
+        print(i)
+        a = cursor.fetchone()
+        Seq = a[0]
+        Age = int(a[1])
+        Ability = int(a[2])
+        Market_Value = a[3]
+
+        potential = 0
+        if Age < 34:
+            if Ability < 50:
+                Ability = Ability + 10
+            for i in range(0, 34-Age, 2):
+                ranpot = random.randrange(Ability, 86)
+                if potential < ranpot:
+                    potential = ranpot
+            if potential > 80:
+                ranpot = random.randrange(Ability, 86)
+                potential = ranpot
+        else:
+            if Ability < 70:
+                Ability = Ability + 4
+            potential = Ability
+
+        if len(Market_Value) == 7:
+            potential += random.randrange(1, 4)
+        cursor1.execute(
+            f"UPDATE Players SET Potential='{potential}'  WHERE Seq == '{Seq}'")
+        db.commit()
+
+
+make_potential()
