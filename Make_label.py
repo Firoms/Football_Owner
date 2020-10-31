c6fea20b4b55430d11d428e8025801943c964e19
Firoms <edisonmessi@naver.com>
2020-10-31 21:31:43 +0900

    2020/10/31

--- Make_label.py
+++ Make_label.py
@@ -20,7 +20,13 @@ class Get_label:
         final_path = os.path.join(img_path, file_name)
         image = ImageTk.PhotoImage(file=final_path)
         image_label = Label(
-            Gui.Gui, text=text, compound=tkinter.CENTER, fg=color, font=font, justify=LEFT)
+            Gui.Gui,
+            text=text,
+            compound=tkinter.CENTER,
+            fg=color,
+            font=font,
+            justify=LEFT,
+        )
         image_label.configure(image=image)
         image_label.image = image
         image_label.configure(text=text)
@@ -31,8 +37,7 @@ class Get_label:
         img_path = os.path.join(os.getcwd(), "images")
         final_path = os.path.join(img_path, file_name)
         image = ImageTk.PhotoImage(file=final_path)
-        image_button = Button(
-            Gui.Gui, overrelief=SOLID,  command=command)
+        image_button = Button(Gui.Gui, overrelief=SOLID, command=command)
         image_button.configure(image=image)
         image_button.image = image
         image_button.place(x=x, y=y)
@@ -43,7 +48,15 @@ class Get_label:
         final_path = os.path.join(img_path, file_name)
         image = ImageTk.PhotoImage(file=final_path)
         image_button = Button(
-            Gui.Gui, overrelief=SOLID,  command=command, text=text, compound=tkinter.CENTER, fg=color, font=font, justify=LEFT)
+            Gui.Gui,
+            overrelief=SOLID,
+            command=command,
+            text=text,
+            compound=tkinter.CENTER,
+            fg=color,
+            font=font,
+            justify=LEFT,
+        )
         image_button.configure(image=image)
         image_button.image = image
         image_button.configure(text=text)
