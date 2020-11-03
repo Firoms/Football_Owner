5f47bfecb8cb11a8885fd88439f89be231a501d7
Firoms <edisonmessi@naver.com>
2020-11-03 20:18:55 +0900

    2020/11/03

--- Get_Api.py
+++ Get_Api.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime
 import sqlite3
 
-API_KEY = 'vt3RYmcPrf4zdoIQLYtV51cx58szBpFL10D757Bc'
+API_KEY = "vt3RYmcPrf4zdoIQLYtV51cx58szBpFL10D757Bc"
 
 
 ###########################################################################
@@ -10,7 +10,8 @@ API_KEY = 'vt3RYmcPrf4zdoIQLYtV51cx58szBpFL10D757Bc'
 ###########################################################################
 def get_league_id():
     r = requests.get(
-        'https://data.football-api.com/v3/competitions?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+        "https://data.football-api.com/v3/competitions?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"
+    )
     data = r.json()
 
     db = sqlite3.connect("DB/new_datafile.db")
@@ -18,13 +19,13 @@ def get_league_id():
 
     for i in range(len(data)):
         leauge_data = data[i]
-        leauge_id = leauge_data['id']
-        leauge_name = leauge_data['name']
-        leauge_region = leauge_data['region']
-        insert_query = \
-            f"INSERT INTO Leagues VALUES('{leauge_id}', '{leauge_name}', '{leauge_region}')"
+        leauge_id = leauge_data["id"]
+        leauge_name = leauge_data["name"]
+        leauge_region = leauge_data["region"]
+        insert_query = f"INSERT INTO Leagues VALUES('{leauge_id}', '{leauge_name}', '{leauge_region}')"
         cursor.execute(insert_query)
         db.commit()
+
 
 ###########################################################################
 # 팀 API id 가져오기
@@ -36,11 +37,13 @@ def get_team_id():
     cursor = db.cursor()
     leauge_id = 1163
     r = requests.get(
-        f'https://data.football-api.com/v3/standings/{leauge_id}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+        f"https://data.football-api.com/v3/standings/{leauge_id}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"
+    )
     data = r.json()
     for i in range(len(data)):
         r = requests.get(
-            f'https://data.football-api.com/v3/standings/{leauge_id}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+            f"https://data.football-api.com/v3/standings/{leauge_id}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"
+        )
         data = r.json()
         Team_data = data[i]
 
@@ -64,17 +67,18 @@ def get_team_id():
         Team_goal_dif = Team_data["gd"]
         Team_points = Team_data["points"]
         r = requests.get(
-            f'https://data.football-api.com/v3/teams/{Team_id}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+            f"https://data.football-api.com/v3/teams/{Team_id}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"
+        )
         data = r.json()[0]
         Team_stadium = data["venue_name"]
         Team_coach = data["coach_name"]
-        insert_query = \
-            f'INSERT INTO China_Teams VALUES("{Team_country}", "{Team_id}", "{Team_name}","{Team_coach}", "{Team_form}", "{Team_position}", "{Team_home_match}",\
+        insert_query = f'INSERT INTO China_Teams VALUES("{Team_country}", "{Team_id}", "{Team_name}","{Team_coach}", "{Team_form}", "{Team_position}", "{Team_home_match}",\
                                             "{Team_home_win}", "{Team_home_draw}", "{Team_home_lose}", "{Team_home_goal}", "{Team_home_goal_lost}",\
                                             "{Team_away_match}", "{Team_away_win}", "{Team_away_draw}", "{Team_away_lose}", "{Team_away_goal}",\
                                             "{Team_away_goal_lost}", "{Team_goal_dif}", "{Team_points}","{Team_stadium}")'
         cursor.execute(insert_query)
         db.commit()
+
 
 ###########################################################################
 # 팀 API 정보 가져오기
@@ -84,26 +88,24 @@ def get_team_id():
 def get_team_data():
     db = sqlite3.connect("DB/new_datafile.db")
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT COUNT(*) FROM SerieA_Teams")
+    cursor.execute("SELECT COUNT(*) FROM SerieA_Teams")
     Team_range = cursor.fetchone()[0]
     Teams = []
     Teams_id = []
 
-    cursor.execute(
-        "SELECT Team_name FROM SerieA_Teams ORDER BY Rank")
+    cursor.execute("SELECT Team_name FROM SerieA_Teams ORDER BY Rank")
     for i in range(Team_range):
         Teams.append(cursor.fetchone()[0])
 
-    cursor.execute(
-        "SELECT Team_id FROM SerieA_Teams")
+    cursor.execute("SELECT Team_id FROM SerieA_Teams")
     for i in range(Team_range):
         Teams_id.append(cursor.fetchone()[0])
 
     for i in range(Team_range):
         print(Teams[i])
         r = requests.get(
-            f'https://data.football-api.com/v3/teams/{Teams_id[i]}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+            f"https://data.football-api.com/v3/teams/{Teams_id[i]}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"
+        )
         player_datas = r.json()[0]["squad"]
         for j in range(len(player_datas)):
 
@@ -122,8 +124,7 @@ def get_team_data():
             Player_yellowred = data["yellowred"]
             Player_redcards = data["redcards"]
 
-            insert_query = \
-                f'INSERT INTO Players VALUES("{Teams[i]}",  "{Player_id}", "{Player_name}", "{Player_number}", "{Player_age}",\
+            insert_query = f'INSERT INTO Players VALUES("{Teams[i]}",  "{Player_id}", "{Player_name}", "{Player_number}", "{Player_age}",\
                                                 "{Player_position}", "{Player_injured}", "{Player_minutes}", "{Player_appearences}",\
                                                 "{Player_goals}", "{Player_assists}", "{Player_yellowcards}", "{Player_yellowred}", "{Player_redcards}")'
             cursor.execute(insert_query)
@@ -137,26 +138,24 @@ def get_team_statistics():
     db = sqlite3.connect("DB/Football_API.db")
     cursor = db.cursor()
 
-    cursor.execute(
-        "SELECT COUNT(*) FROM Team_Datas")
+    cursor.execute("SELECT COUNT(*) FROM Team_Datas")
     Team_range = cursor.fetchone()[0]
     Teams = []
     Teams_id = []
 
-    cursor.execute(
-        "SELECT name FROM Team_Datas ORDER BY position")
+    cursor.execute("SELECT name FROM Team_Datas ORDER BY position")
     for i in range(Team_range):
         Teams.append(cursor.fetchone()[0])
 
-    cursor.execute(
-        "SELECT id FROM Team_Datas")
+    cursor.execute("SELECT id FROM Team_Datas")
 
     for i in range(Team_range):
         Teams_id.append(cursor.fetchone()[0])
 
     for i in range(16, Team_range):
         r = requests.get(
-            f'https://data.football-api.com/v3/teams/{Teams_id[i]}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+            f"https://data.football-api.com/v3/teams/{Teams_id[i]}?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"
+        )
         data = r.json()[0]["statistics"]
         Player_id = data["avg_goals_per_game_scored_home"]
         Player_name = data["avg_goals_per_game_scored_away"]
@@ -169,8 +168,7 @@ def get_team_statistics():
         Player_goals = data["scoring_minutes_60_75_pct"]
         Player_assists = data["scoring_minutes_75_90_pct"]
 
-        insert_query = \
-            f"INSERT INTO Statistics VALUES('{Teams[i]}',  '{Player_id}', '{Player_name}', '{Player_number}', '{Player_age}',\
+        insert_query = f"INSERT INTO Statistics VALUES('{Teams[i]}',  '{Player_id}', '{Player_name}', '{Player_number}', '{Player_age}',\
                                             '{Player_position}', '{Player_injured}', '{Player_minutes}', '{Player_appearences}',\
                                             '{Player_goals}', '{Player_assists}')"
         cursor.execute(insert_query)
