0babf6b621810d77b1b3afd4b92ac81f20cc2311
Firoms <edisonmessi@naver.com>
2020-12-31 23:41:04 +0900

    2020/12/31

--- practice.py
+++
@@ -1,5 +0,0 @@
-def play_simulation_game(Home, Away, db):
-    Home_Team_manager = team_manager_ability(Home)
-    Away_Team_manager = team_manager_ability(Away)
-    Home_Team_players = team_ability(Home)
-    Away_Team_players = team_ability(Away)
